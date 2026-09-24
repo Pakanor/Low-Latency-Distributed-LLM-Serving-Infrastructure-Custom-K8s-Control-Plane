1635ecbae6b8770c6ad99945f300e9a97eac86fa
Robert <robert.p3@o2.pl>
2026-09-24 17:27:27 +0200

    test: replace K8s integration test with MockModelClient unit test

--- tests/test_k8s_integration.py
+++ tests/test_k8s_integration.py
@@ -2,11 +2,10 @@ import pytest
 from app.memory.manager import PagedKVCacheManager
 from app.scheduler.scheduler import Scheduler
 from app.engine.llm_engine import LLMEngine
-from app.model.client import K8sModelClient
+from app.model.client import MockModelClient
 
 
-def test_live_k8s_model_generation():
-   
+def test_mock_model_generation():
     kv_mgr = PagedKVCacheManager(
         total_blocks=10,
         block_size=4,
@@ -21,11 +20,7 @@ def test_live_k8s_model_generation():
         allocator=kv_mgr.get_allocator()
     )
 
-   
-    client = K8sModelClient(
-        endpoint_url="http://localhost:8000/generate_step",
-        model_name="HuggingFaceTB/SmolLM-135M-Instruct"
-    )
+    client = MockModelClient()
 
     engine = LLMEngine(
         kv_cache_manager=kv_mgr,
@@ -33,10 +28,9 @@ def test_live_k8s_model_generation():
         model_client=client
     )
 
-    prompt_tokens = client.tokenizer.encode("Hello", add_special_tokens=False)
-    seq = engine.add_request(prompt_token_ids=prompt_tokens, max_tokens=3)
+    seq = engine.add_request(prompt_token_ids=[1, 2, 3], max_tokens=3)
 
     res = engine.step()
 
     assert len(seq.output_token_ids) == 1
-    assert seq.output_token_ids[0] != client.tokenizer.eos_token_id
+    assert seq.output_token_ids[0] == 103 
